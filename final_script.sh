2f4c724eac2c7cb25c3a9090d8a412db387f9a36
Lara Skuppin <140449478+larixlarix@users.noreply.github.com>
2023-09-04 21:48:03 +0200

    adapted final_script to new folder structure

--- final_script.sh
+++ final_script.sh
@@ -6,13 +6,13 @@ source .venv/bin/activate
 ## Run the python scripts
 
 # cleaning the data
-python3 main_cleaning_notebook.py
+python3 src/main_cleaning_notebook.py
 
 # feature engineering etc.
-python3 data_wrangling.py
+python3 src/data_wrangling.py
 
 # baseline model
-python3 models_baseline.py
+python3 src/models_baseline.py
 
 # best model for our case
-python3 Miauboost.py
+python3 src/Miauboost.py
